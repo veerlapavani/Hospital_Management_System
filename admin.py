f0601994c25ac17c73b584ae044acde78a90d3d8
veerlapavani <veerlapavani754@gmail.com>
2025-03-08 20:50:58 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Doctor,Department,Patient,Appointment
-
-# Register your models here.
-admin.site.register((Department,Doctor,Patient,Appointment))
